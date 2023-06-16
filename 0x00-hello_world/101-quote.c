855b6fb0dda6ceb1e9414f9a9eb3e8ec1469efa4
Bernard139 <aghedob36@gmail.com>
2023-06-16 03:22:17 +0100

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - "
-				"Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\"n"
+		"- Dora Korpar, 2015-10-19\n";
 
-	write(STDOUT_FILENO, message, sizeof(message) - 1);
-
+	write(2, message, 59);
 	return (1);
 }

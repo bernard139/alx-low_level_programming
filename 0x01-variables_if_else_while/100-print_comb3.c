3c4ff0aaa0b63420fe7e2824acb2afc14218da48
Bernard139 <aghedob36@gmail.com>
2023-06-19 01:01:29 +0100

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 			if (c < i)
 			{
 				putchar(c);
-				putchar(c);
+				putchar(i);
 			}
 			if (c != '8' || (c == '8' && i != '9'))
 				{
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints 3 combination of numbers
- *     *
- *       * Return: Always (Success)
- *         */
+ * main - Prints 3 combination of numbers
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int c, i, k, j;
@@ -13,31 +13,31 @@ int main(void)
 	{
 		for (i = 48; i <= 57; i++)
 		{
-			for (k = 48; k <= 57; k++)
+		for (k = 48; k <= 57; k++)
+		{
+		for (j = 48; j <= 57; j++)
+		{
+			if (((k + j) > (c + i) &&  k >= c) || c < k)
 			{
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+				putchar(c);
+				putchar(i);
+				putchar(' ');
+				putchar(k);
+				putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-						break;
-						}
-						else
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
-				}
+			if (c + i + k + j == 227 && c == 57)
+			{
+			break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			}
 		}
+		}
+	}
 	}
 
 	putchar('\n');

85b7a3a51860e980cd9b07fa00083fda53249707
Bernard139 <aghedob36@gmail.com>
2023-06-19 01:09:04 +0100

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,8 +17,8 @@ int main(void)
 			{
 				putchar(c);
 				putchar(i);
-			
-			if (c != '8' || (c == '8' && i != '9'))
+
+			if (c != '9' || (c == '9' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');

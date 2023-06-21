8d5f37a9e8b417c1ca744fd25023672b14012871
Bernard139 <aghedob36@gmail.com>
2023-06-21 16:12:22 +0100

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10 lowercase
+ *
+ * Return: Always(success)
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		for(x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	}
+
+	_putchar('\n');
+
+	return(0);
+}

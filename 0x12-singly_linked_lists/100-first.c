f71e4e9714768412dc07f8932ff407187c801383
Bernard139 <aghedob36@gmail.com>
2023-09-21 22:22:32 +0100

    new

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
- * premain - prints a string before the main function is executed
+ * first - ...
+ *
+ * Return: Nothing.
  */
-__attribute__ ((constructor))
-	void premain(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,60 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to be measured
+ * add_node_end - Adds a new node at the end of a list
+ * @head: The original linked list
+ * @str: The string to add to the node
  *
- * Return: the length of the string
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	i++;
-	return (i);
-}
-
-/**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to the head of the list
- * @str: string to be duplicated
- *
- * Return: the address of the new element, or NULL if it failed
+ * Return: The address of the new list or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *last;
+	list_t *new_list, *temp;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	return (NULL);
-		new->str = strdup(str);
-		new->len = _strlen(str);
-		new->next = NULL;
-	if (*head == NULL)
-		*head = new;
-	else
+	if (str != NULL)
 	{
-			last = *head;
-		while (last->next)
-			last = last->next;
-			last->next = new;
+			new_list = malloc(sizeof(list_t));
+		if (new_list == NULL)
+		return (NULL);
+
+		new_list->str = strdup(str);
+		new_list->len = _strlen(str);
+		new_list->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head  = new_list;
+			return (*head);
+		}
+		else
+		{
+				temp = *head;
+			while (temp->next)
+				temp = temp->next;
+				temp->next = new_list;
+			return (temp);
+		}
 	}
-	return (new);
+	return (NULL);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+	return (c);
 }
